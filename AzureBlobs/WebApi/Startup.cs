f09246406a073574e8c43c22ca35227b1c9d9a17
Behzad Karim <bekarim@microsoft.com>
2019-07-29 18:48:08 -0700

    Fixed a bug in DI.

--- AzureBlobs/WebApi/Startup.cs
+++ AzureBlobs/WebApi/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AzureBlobsCore.BlobStorage;
+using AzureBlobsCore.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -16,17 +17,17 @@ namespace WebApi
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
-
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IBlobStorageRepository, BlobStorageRepository>();
+            services.AddTransient<IBlobStorageRepository, BlobStorageRepository>(p => new BlobStorageRepository(AppSettings.LocalDevConnectionString));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
